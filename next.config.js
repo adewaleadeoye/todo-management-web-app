d7da01423ab25bba6c2a4a55cf57cb08a728f3de
Adewale Adeoye <whaale_ade@yahoo.com>
2021-09-29 02:15:21 +0100

    nextjs native xss protection

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+];
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
